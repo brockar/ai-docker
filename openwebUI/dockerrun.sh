b578df8df4a2967b3775b821aa1771ef58dd0f2a
brockar <martinnguzman.mg@gmail.com>
2024-11-14 16:30:52 -0300

    todo

--- openwebUI/dockerrun.sh
+++ openwebUI/dockerrun.sh
@@ -2,4 +2,6 @@
 docker run -d -p 3000:8080 --gpus all --add-host=host.docker.internal:host-gateway \
   -v open-webui:/app/backend/data --name open-webui \
   --name openwebui \
-  --restart always ghcr.io/open-webui/open-webui:cuda
+# --restart always 
+  ghcr.io/open-webui/open-webui:cuda
+
---
+++ todo.md
@@ -0,0 +1,1 @@
+- [ ] make specific paths to volumes
